f7e41224b54e1fb4370d4e2110a3cb460989c7b8
Klimov Paul <klimov.paul@gmail.com>
2015-05-08 16:18:48 +0300

    `MongoIdValidator` created

---
+++ tests/validators/MongoIdValidatorTest.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace yiiunit\extensions\mongodb\validators;
+
+use yii\base\Model;
+use yii\mongodb\validators\MongoIdValidator;
+use yiiunit\extensions\mongodb\TestCase;
+
+class MongoIdValidatorTest extends TestCase
+{
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->mockApplication();
+    }
+
+    public function testValidateValue()
+    {
+        $validator = new MongoIdValidator();
+        $this->assertFalse($validator->validate('id'));
+        $this->assertTrue($validator->validate(new \MongoId('4d3ed089fb60ab534684b7e9')));
+        $this->assertTrue($validator->validate('4d3ed089fb60ab534684b7e9'));
+    }
+
+    public function testValidateAttribute()
+    {
+        $model = new MongoIdTestModel();
+        $validator = new MongoIdValidator();
+        $validator->attributes = ['id'];
+        $model->getValidators()->append($validator);
+
+        $model->id = 'id';
+        $this->assertFalse($model->validate());
+        $model->id = new \MongoId('4d3ed089fb60ab534684b7e9');
+        $this->assertTrue($model->validate());
+        $model->id = '4d3ed089fb60ab534684b7e9';
+        $this->assertTrue($model->validate());
+    }
+
+    /**
+     * @depends testValidateAttribute
+     */
+    public function testConvertValue()
+    {
+        $model = new MongoIdTestModel();
+        $validator = new MongoIdValidator();
+        $validator->attributes = ['id'];
+        $model->getValidators()->append($validator);
+
+        $validator->forceFormat = null;
+        $model->id = '4d3ed089fb60ab534684b7e9';
+        $model->validate();
+        $this->assertTrue(is_string($model->id));
+        $model->id = new \MongoId('4d3ed089fb60ab534684b7e9');
+        $model->validate();
+        $this->assertTrue($model->id instanceof \MongoId);
+
+        $validator->forceFormat = 'object';
+        $model->id = '4d3ed089fb60ab534684b7e9';
+        $model->validate();
+        $this->assertTrue($model->id instanceof \MongoId);
+
+        $validator->forceFormat = 'string';
+        $model->id = new \MongoId('4d3ed089fb60ab534684b7e9');
+        $model->validate();
+        $this->assertTrue(is_string($model->id));
+    }
+}
+
+class MongoIdTestModel extends Model
+{
+    public $id;
+}
---
+++ validators/MongoIdValidator.php
@@ -0,0 +1,113 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace yii\mongodb\validators;
+
+use yii\base\InvalidConfigException;
+use yii\validators\Validator;
+use Yii;
+
+/**
+ * MongoIdValidator verifies if the attribute is a valid Mongo ID.
+ * Attribute will be considered as valid, if it is an instance of [[\MongoId]] or a its string value.
+ *
+ * Usage example:
+ *
+ * ~~~
+ * class Customer extends yii\mongodb\ActiveRecord
+ * {
+ *     ...
+ *     public function rules()
+ *     {
+ *         return [
+ *             ['_id', 'yii\mongodb\validators\MongoIdValidator']
+ *         ];
+ *     }
+ * }
+ * ~~~
+ *
+ * This validator may also serve as a filter, allowing conversion of Mongo ID value either to the plain string
+ * or to [[\MongoId]] instance. You can enable this feature via [[forceFormat]].
+ *
+ * @author Paul Klimov <klimov.paul@gmail.com>
+ * @since 2.0.4
+ */
+class MongoIdValidator extends Validator
+{
+    /**
+     * @var string|null specifies the format, which validated attribute value should be converted to
+     * in case validation was successful.
+     * valid values are:
+     * - 'string' - enforce value converted to plain string.
+     * - 'object' - enforce value converted to [[\MongoId]] instance.
+     * If not set - no conversion will be performed, leaving attribute value intact.
+     */
+    public $forceFormat;
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+        if ($this->message === null) {
+            $this->message = Yii::t('yii', '{attribute} is invalid.');
+        }
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function validateAttribute($model, $attribute)
+    {
+        $value = $model->$attribute;
+        $mongoId = $this->parseMongoId($value);
+        if (is_object($mongoId)) {
+            if ($this->forceFormat !== null) {
+                switch ($this->forceFormat) {
+                    case 'string' : {
+                        $model->$attribute = $mongoId->__toString();
+                        break;
+                    }
+                    case 'object' : {
+                        $model->$attribute = $mongoId;
+                        break;
+                    }
+                    default: {
+                        throw new InvalidConfigException("Unrecognized format '{$this->forceFormat}'");
+                    }
+                }
+            }
+        } else {
+            $this->addError($model, $attribute, $this->message, []);
+        }
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function validateValue($value)
+    {
+        return is_object($this->parseMongoId($value)) ? null : [$this->message, []];
+    }
+
+    /**
+     * @param mixed $value
+     * @return \MongoId|null
+     */
+    private function parseMongoId($value)
+    {
+        if ($value instanceof \MongoId) {
+            return $value;
+        }
+        try {
+            return new \MongoId($value);
+        } catch (\Exception $e) {
+            return null;
+        }
+    }
+} 
