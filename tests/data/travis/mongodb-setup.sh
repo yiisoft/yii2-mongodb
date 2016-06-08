39788c78855bf2d4a2a02453b9a354a8b5947bf1
Klimov Paul <klimov.paul@gmail.com>
2016-06-08 13:28:16 +0300

    travis changed to install MongoDB server manually

--- tests/data/travis/mongodb-setup.sh
+++ tests/data/travis/mongodb-setup.sh
@@ -2,14 +2,25 @@
 #
 # install mongodb
 
-pecl install -f mongodb
+# MongoDB Server :
 
-/usr/bin/mongo yii2test --eval 'db.createUser({user: "travis", pwd: "test", roles: ["readWrite", "dbAdmin"]});'
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+echo "deb http://repo.mongodb.org/apt/ubuntu precise/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+sudo apt-get update -qq
+sudo apt-get install -y mongodb-org
 
-echo "extension = mongodb.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
+sudo service mongod start
 
 echo "MongoDB Server version:"
 mongod --version
+
+mongo yii2test --eval 'db.createUser({user: "travis", pwd: "test", roles: ["readWrite", "dbAdmin"]});'
+
+# PHP Extension :
+
+pecl install -f mongodb
+
+echo "extension = mongodb.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
 
 echo "MongoDB PHP Extension version:"
 php -i |grep mongodb -4 |grep -2 Version
