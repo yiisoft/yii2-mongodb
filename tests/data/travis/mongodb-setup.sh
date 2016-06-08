910add873a8a2954c544dc5e738d31d42a43d65a
Klimov Paul <klimov.paul@gmail.com>
2016-06-08 14:17:50 +0300

    `mongodb-setup.sh` fix

--- tests/data/travis/mongodb-setup.sh
+++ tests/data/travis/mongodb-setup.sh
@@ -16,6 +16,6 @@ pecl install -f mongodb
 echo "extension = mongodb.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
 
 echo "MongoDB PHP Extension version:"
-php -i |grep mongodb -4 |grep -2 Version
+php -i |grep mongodb -4 |grep -2 mongodb version
 
-cat /etc/mongodb.conf
+#cat /etc/mongodb.conf
