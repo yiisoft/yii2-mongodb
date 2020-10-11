85e0c79fceae5f5cde0c414d910e3a9f52ae4822
Ziaratban <abolfazl.ziaratban@gmail.com>
2020-10-11 19:21:22 +0330

    Update mongodb-setup.sh

--- tests/data/travis/mongodb-setup.sh
+++ tests/data/travis/mongodb-setup.sh
@@ -12,12 +12,26 @@ mongo yii2test --eval 'db.createUser({user: "travis", pwd: "test", roles: ["read
 # PHP Extension :
 
 if (php --version | grep -i HipHop > /dev/null); then
-  echo "skip PHP extension installation on HHVM"
+    echo "skip PHP extension installation on HHVM"
 else
-  pecl install -f mongodb
-  echo "extension = mongodb.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
-  echo "MongoDB PHP Extension version:"
-  php -i |grep mongodb -4 |grep -2 version
+
+    #https://docs.mongodb.com/drivers/php#language-compatibility
+    if [ $(php -r "echo version_compare(phpversion(),'5.5','<') ? 1 : 0;") == 1 ]
+    then
+        pecl install -f mongodb-1.3.4
+    elif [ $(php -r "echo version_compare(phpversion(),'5.5','>=') && version_compare(phpversion(),'7.2','<=') ? 1 : 0;") == 1 ]
+    then
+        pecl install -f mongodb-1.4.4
+    elif [ $(php -r "echo version_compare(phpversion(),'7.3','>=') && version_compare(phpversion(),'7.4','<') ? 1 : 0;") == 1 ]
+    then
+        pecl install -f mongodb-1.5.5
+    elif [ $(php -r "echo version_compare(phpversion(),'7.4','>=') ? 1 : 0;") == 1 ]
+    then
+        pecl install -f mongodb
+    fi
+    echo "extension = mongodb.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
+    echo "MongoDB PHP Extension version:"
+    php -i |grep mongodb -4 |grep -2 version
 fi
 
 #cat /etc/mongodb.conf
