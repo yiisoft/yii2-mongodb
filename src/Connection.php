6c6e71f5dd0c1e7681a48657a523ba5676b7ba4f
Metola <alexmetola@hotmail.com>
2021-01-10 22:27:58 +0100

    Update Connection.php for add +srv
    Fix issue #304

--- src/Connection.php
+++ src/Connection.php
@@ -200,7 +200,7 @@ class Connection extends Component
     public function getDefaultDatabaseName()
     {
         if ($this->_defaultDatabaseName === null) {
-            if (preg_match('/^mongodb:\\/\\/.+\\/([^?&]+)/s', $this->dsn, $matches)) {
+            if (preg_match('/^mongodb(:|\+srv:)\\/\\/.+\\/([^?&]+)/s', $this->dsn, $matches)) {
                 $this->_defaultDatabaseName = $matches[1];
             } else {
                 throw new InvalidConfigException("Unable to determine default database name from dsn.");
